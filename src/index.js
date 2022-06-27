4c5ff58e58d3825e1558b442dcbbd67a8988d072
BogdanV <zarazailo@gmail.com>
2022-06-27 23:55:42 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from "./App";
-import 'modern-normalize/modern-normalize.css'
+import { App } from 'components/App';
 import './index.css';
-
-import user from './data/social-profile.json';
-import statistics from './data/statistics.json';
-import friends from './data/friend-list.json';
-import transactions from './data/transaction-history.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App data={{ user, statistics, friends, transactions }} />
+    <App />
   </React.StrictMode>
 );
